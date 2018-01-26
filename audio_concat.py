b981e95a867e0544be646153d3d8c292f10acfe1
Kalugin Dima <kalugin.di@phystech.edu>
2018-01-27 02:59:46 +0300

    concat

---
+++ audio_concat.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from gtts import gTTS
+from pydub import AudioSegment
+import sox
+
+words = [u"Эй",u"клач",u"плач", u"мурзач"]
+wavs = [w+".wav" for w in words]
+
+def save_tts(words):
+    for w in words:
+        print "Processing word ",w
+        tts = gTTS(text=w,lang='ru',slow=True)
+        tts.save(w+'.mp3');
+        print "got tts result"
+        waud = AudioSegment.from_mp3(w+'.mp3')
+        waud.export(w+".wav", format="wav")
+
+# create combiner
+cbn = sox.Combiner()
+# pitch shift combined audio up 3 semitones
+cbn.pitch(3.0)
+# convert output to 8000 Hz stereo
+cbn.convert(samplerate=8000)
+# create the output file
+cbn.build(wavs, 'output.wav', 'concatenate')
+
+sound1 = AudioSegment.from_file("output.wav")
+sound2 = AudioSegment.from_file("beat.wav")
+
+combined = sound1.overlay(sound2)
+
+combined.export("combined.wav", format='wav')
