a04af050d215f7a8217ee447d57a064c6a710df7
Nadezhda Zueva <zueva.nn@phystech.edu>
2018-01-27 19:31:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,6 +27,14 @@ def echo(bot, update):
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
+def help_message(arguments, message):
+    response = {'chat_id': message['chat']['id']}
+    result = ["Hey, %s!" % message["from"].get("first_name"),
+              "\rI can accept only these commands:"]
+    for command in CMD:
+        result.append(command)
+    response['text'] = "\n\t".join(result)
+    return response
 
 def main():
     updater = Updater(TELEGRAM_TOKEN)
