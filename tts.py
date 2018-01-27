bc5d9b1d000ec7263bb51fe3b8d95d8bd09dcaee
DaniloZZZ <lkv97dn@gmail.com>
2018-01-27 10:48:43 +0300

    trying to read lyr from file

--- tts.py
+++ tts.py
@@ -2,14 +2,22 @@
 # -*- coding: utf-8 -*-
 from gtts import gTTS
 from pydub import AudioSegment
+import codecs
+import numpy as np
 import json
 import sox
 
-
 words= json.load(open('words.json'))
+lyr = codecs.open('lyrics.txt',encoding='utf-8')
+words=lyr.read().replace(",", "").split(' ')
+#print np.array(words).astype('U')
+print (words)
+#print [w.encode('utf-8') for w in words]
+lyr.close()
+
 wavs = [w+".wav" for w in words]
 
-def save_tts(words):
+def save_tts(words):    
     for w in set(words):
         print "Processing word ",w
         tts = gTTS(text=w,lang='ru',slow=False)
